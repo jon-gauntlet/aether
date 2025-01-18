16c5ee36ec814b93a4c247124111aa94b2e5c6ae
Jon Dycaico <jdycaico@tutanota.com>
2025-01-18 11:07:13 -0800

    chore: simplify repo quality tools
    - Update ESLint config with better rules - Add EditorConfig for consistent style - Simplify pre-commit setup - Add accessibility rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,15 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    jest: true,
+    'jest/globals': true,
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
@@ -22,11 +26,64 @@ module.exports = {
       version: 'detect',
     },
   },
-  plugins: ['react'],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'jest'],
   rules: {
+    // React rules
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
-    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'react/jsx-no-target-blank': 'error',
+    'react/jsx-uses-react': 'off',
+    'react/jsx-uses-vars': 'error',
+    'react/no-unknown-property': 'error',
+
+    // Hooks rules
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // General rules
+    'no-unused-vars': ['warn', { 
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_',
+    }],
+    'no-console': ['warn', { 
+      allow: ['warn', 'error'] 
+    }],
+    'no-debugger': 'warn',
+    'no-duplicate-imports': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'eqeqeq': ['error', 'always'],
+
+    // Accessibility rules
+    'jsx-a11y/alt-text': 'error',
+    'jsx-a11y/aria-role': 'error',
+    'jsx-a11y/aria-props': 'error',
+    'jsx-a11y/aria-proptypes': 'error',
+    'jsx-a11y/role-has-required-aria-props': 'error',
   },
+  overrides: [
+    {
+      files: [
+        '**/*.test.js',
+        '**/*.test.jsx',
+        '**/__tests__/**',
+        '**/mocks/**',
+        '**/setup.{js,jsx}',
+      ],
+      env: {
+        jest: true,
+        'jest/globals': true,
+      },
+      rules: {
+        'no-unused-expressions': 'off',
+        'react/prop-types': 'off',
+        'react/display-name': 'off',
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+      },
+    },
+  ],
 }; 
